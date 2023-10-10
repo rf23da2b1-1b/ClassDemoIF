cf0bca936789fd6eb4ec5aae140672ed4f8eb4b0
Peter Levinsky <pele@zealand.dk>
2023-10-10 13:01:18 +0200

    Add project files.

---
+++ ClassDemoIF/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int pris = 20;
+
+if (pris > 30)
+{
+    if (pris < 60)
+    {
+        Console.WriteLine("pris mellem 30 og 60");
+    }
+    Console.WriteLine("Ja prisen er større end 60");
+    //Console.WriteLine("Endnu en linje");
+}
+else
+{
+    Console.WriteLine("Nej prisen er under eller lig med 30");
+}
+
+Console.WriteLine("dette er endnu en linje");
