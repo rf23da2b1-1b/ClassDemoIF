3c25445c83eb1a5e0a6ff7bf69a0573f3917f49a
Peter Levinsky <pele@zealand.dk>
2023-10-10 14:18:29 +0200

    Med swich

--- ClassDemoIF/Program.cs
+++ ClassDemoIF/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
 int pris = 20;
@@ -18,3 +20,47 @@ else
 }
 
 Console.WriteLine("dette er endnu en linje");
+
+
+Console.Clear();
+Console.WriteLine("Menu");
+Console.WriteLine("1: Opret");
+Console.WriteLine("2: Slet");
+Console.WriteLine("3: Hent");
+Console.WriteLine("Indtast valg: ");
+
+String valg = Console.ReadLine();
+
+/*
+ * Læs valg med if
+ */
+if (valg == "1")
+{
+    Console.WriteLine("Du har valgt nummer 1");
+}
+else
+if (valg == "2")
+{
+    Console.WriteLine("Du har valgt nummer 2");
+}
+else
+if (valg == "3")
+{
+    Console.WriteLine("Du har valgt nummer 3");
+}
+else
+{
+    Console.WriteLine($"Du har valgt {valg} med det er ikke understøttet");
+}
+
+/*
+ * Læs valg med switch
+ */
+//switch (valg)
+//{
+//    case "1": Console.WriteLine("Du har valgt nummer 1"); break;
+//    case "2": Console.WriteLine("Du har valgt nummer 2"); break;
+//    case "3": Console.WriteLine("Du har valgt nummer 3"); break;
+
+//    default: Console.WriteLine($"Du har valgt {valg} med det er ikke understøttet");break;
+//}
